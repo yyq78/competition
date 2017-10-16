771a37690d25ad754734436bae46ce7c28ce97ad
yyq <1453582072@qq.com>
2017-10-16 23:15:18 +0800

    modify main.js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -3,18 +3,26 @@ $(document).ready(function(){
         var $selectedValue=$("input[name='options-number']:checked").val();
         if($selectedValue==1)
         {
+            $("fieldset #person2").attr("disabled",true);
+            $("fieldset #person3").attr("disabled",true);
             $("#person2").hide();
             $("#person3").hide();
         }
         if($selectedValue==2)
         {
+            $("fieldset #person2").attr("disabled",false);
+            $("fieldset #person3").attr("disabled",true);
             $("#person2").show();
             $("#person3").hide();
         }
         if($selectedValue==3)
         {
+
             $("#person2").show();
             $("#person3").show();
+            $("fieldset #person2").attr("disabled",false);
+            $("fieldset #person3").attr("disabled",false);
+
         }
     });
 });
